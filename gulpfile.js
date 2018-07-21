0c81a7787cf72fe2bef0512bdd2dc42c9f48efb2
Michael Schuster <development@unltd-networx.de>
2018-07-21 09:37:30 +0200

    Rename to energymanager

--- gulpfile.js
+++ gulpfile.js
@@ -373,8 +373,8 @@ gulp.task('updatePackages', function (done) {
 
 gulp.task('rename', function ()  {
     var newname;
-    var author = '@@Author@@';
-    var email  = '@@email@@';
+    var author = 'Michael Schuster';
+    var email  = 'development@unltd-networx.de';
     for (var a = 0; a < process.argv.length; a++) {
         if (process.argv[a] === '--name') {
             newname = process.argv[a + 1]
@@ -388,7 +388,7 @@ gulp.task('rename', function ()  {
 
     console.log('Try to rename to "' + newname + '"');
     if (!newname) {
-        console.log('Please write the new template name, like: "gulp rename --name mywidgetset" --author "Author Name"');
+        console.log('Please write the new energymanager name, like: "gulp rename --name mywidgetset" --author "Author Name"');
         process.exit();
     }
     if (newname.indexOf(' ') !== -1) {
@@ -399,37 +399,37 @@ gulp.task('rename', function ()  {
         console.log('Name must be lower case.');
         process.exit();
     }
-    if (fs.existsSync(__dirname + '/admin/template.png')) {
-        fs.renameSync(__dirname + '/admin/template.png',              __dirname + '/admin/' + newname + '.png');
+    if (fs.existsSync(__dirname + '/admin/energymanager.png')) {
+        fs.renameSync(__dirname + '/admin/energymanager.png',              __dirname + '/admin/' + newname + '.png');
     }
-    if (fs.existsSync(__dirname + '/widgets/template.html')) {
-        fs.renameSync(__dirname + '/widgets/template.html',           __dirname + '/widgets/' + newname + '.html');
+    if (fs.existsSync(__dirname + '/widgets/energymanager.html')) {
+        fs.renameSync(__dirname + '/widgets/energymanager.html',           __dirname + '/widgets/' + newname + '.html');
     }
-    if (fs.existsSync(__dirname + '/widgets/template/js/template.js')) {
-        fs.renameSync(__dirname + '/widgets/template/js/template.js', __dirname + '/widgets/template/js/' + newname + '.js');
+    if (fs.existsSync(__dirname + '/widgets/energymanager/js/energymanager.js')) {
+        fs.renameSync(__dirname + '/widgets/energymanager/js/energymanager.js', __dirname + '/widgets/energymanager/js/' + newname + '.js');
     }
-    if (fs.existsSync(__dirname + '/widgets/template')) {
-        fs.renameSync(__dirname + '/widgets/template',                __dirname + '/widgets/' + newname);
+    if (fs.existsSync(__dirname + '/widgets/energymanager')) {
+        fs.renameSync(__dirname + '/widgets/energymanager',                __dirname + '/widgets/' + newname);
     }
     var patterns = [
         {
-            match: /ioBroker template Adapter/g,
+            match: /energymanager/g,
             replacement: newname
         },
         {
-            match: /template/g,
+            match: /energymanager/g,
             replacement: newname
         },
         {
-            match: /Template/g,
-            replacement: newname ? (newname[0].toUpperCase() + newname.substring(1)) : 'Template'
+            match: /Energymanager/g,
+            replacement: newname ? (newname[0].toUpperCase() + newname.substring(1)) : 'Energymanager'
         },
         {
-            match: /@@Author@@/g,
+            match: /Michael Schuster/g,
             replacement: author
         },
         {
-            match: /@@email@@/g,
+            match: /development@unltd-networx.de/g,
             replacement: email
         }
     ];
